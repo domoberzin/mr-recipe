434e9e13ab553be812950c6dee4d8a55d19ff8d3
Dominic Berzin <berzindominic@gmail.com>
2023-01-14 16:30:39 +0800

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './MainPage.css';
 
-function App() {
+const MainPage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const resultsPerPage = 3;
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    // Make API call to get search results based on searchTerm
+    // Assume the API call returns an array of recipes
+    // const results = await yourApiCallFunction(searchTerm);
+    const results = [];
+    setSearchResults(results);
+    setCurrentPage(1);
+  }
+
+  const handlePagination = (direction) => {
+    if (direction === 'prev') {
+      setCurrentPage(currentPage - 1);
+    } else {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  const startIndex = (currentPage - 1) * resultsPerPage;
+  const endIndex = startIndex + resultsPerPage;
+  const currentResults = searchResults.slice(startIndex, endIndex);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='main-page'>
+      <h1>Welcome to Cocktail Central</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type='text'
+          placeholder='Search for a recipe'
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button type='submit'>Search</button>
+      </form>
+      <div className='search-results'>
+        {currentResults.map((recipe, index) => (
+          <div key={index}>
+            <h2>{recipe.name}</h2>
+            <p>{recipe.ingredients}</p>
+            <p>{recipe.instructions}</p>
+          </div>
+        ))}
+      </div>
+      <div className='pagination'>
+        <button
+          className='pagination-button'
+          disabled={currentPage === 1}
+          onClick={() => handlePagination('prev')}
         >
-          Learn React
-        </a>
-      </header>
+          Prev
+        </button>
+        <span className='pagination-info'>
+          Page {currentPage} of {Math.ceil(searchResults.length / resultsPerPage)}
+        </span>
+        <button
+          className='pagination-button'
+          disabled={currentPage === Math.ceil(searchResults.length / resultsPerPage)}
+          onClick={() => handlePagination('next')}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default MainPage;
